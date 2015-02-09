0c94ef76b9975ba8a546b10d9f48706186e4a8a3
Clint Chilcott <clintchil@Clints-MacBook-Air.local>
2015-02-09 11:31:02 -0800

    Initial Commit

---
+++ Sub7/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sub7
+//
+//  Created by Clint Chilcott on 2/9/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
