686292050473d8d795a5ad5b818efe3ba7e4ed72
Joseph Lynch <josephlynch@Josephs-MacBook-Pro.local>
2015-02-11 14:29:32 -0800

    infinite scroll downward

---
+++ Sub7/SandwichCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SandwichCollectionViewCell.h
+//  Sub7
+//
+//  Created by Joseph Lynch on 2/10/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SandwichCollectionViewCell : UICollectionViewCell
+
+@property IBOutlet UIImageView *imageView;
+
+
+@end
